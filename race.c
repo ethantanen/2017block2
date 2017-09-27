e62d119e843409af6f76aaa1c0e9bab88d9325a4
ethan <ethantanen@yahoo.com>
2017-09-27 15:59:27 -0600

    unedited race.c

---
+++ race.c
@@ -0,0 +1,30 @@
+/*
+ * race.c
+ *
+ *  Created on: Sep 27, 2017
+ *      Author: ethantanen
+ */
+
+#include <omp.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    size_t T=8;
+    int t_hello[T];
+    int i=0;
+    for(i=0;i<T;i++) {
+        t_hello[i]=-1;
+    }
+    i=0;
+    #pragma omp parallel num_threads(8)
+    {
+        t_hello[i]=omp_get_thread_num();
+        i++;
+    }
+
+    for(i=0;i<T;i++) {
+        printf("Hello from %d\n",t_hello[i]);
+    }
+
+    return 0;
+}
