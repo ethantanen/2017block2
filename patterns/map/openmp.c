577a82575265924fe51aa95100ed7f88387c91e8
ethan <ethantanen@yahoo.com>
2017-10-02 11:31:27 -0600

    add break

--- patterns/map/openmp.c
+++ patterns/map/openmp.c
@@ -101,7 +101,7 @@ int main(int argc, char** argv) {
             notfound = test(argv[1],passmatch);
             
             if(notfound == 0){
-                return;
+                break;
             }
         }
         
