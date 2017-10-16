615b61077afd40b4c454a4d95df7545c4ce96493
ethan <ethantanen@yahoo.com>
2017-10-15 21:39:34 -0600

    updated openmp.h

--- project/src/openmp.h
+++ project/src/openmp.h
@@ -11,4 +11,24 @@
 
 #include <stdio.h>
 
+#define MNIST_DOUBLE
+#define USE_MNIST_LOADER
+#define MNIST_STATIC
+#include "mnist.h"
+#include <time.h>
+
+
+extern const int in;
+extern const int hid;
+extern const int out;
+
+double sigmoid(double activation);
+double sig_prime(double activation);
+int get_mnist(double *input,double *target);
+int save_net(double weights_ih[in+1][hid+1],double weights_ho[hid+1][out+1],char *file_name);
+long get_elapsed_time(struct timespec start_time,struct timespec end_time);
+int train_net(double weights_ih[in+1][hid+1],double weights_ho[hid+1][out+1],double **images, double **targets,int thread_id);
+int train_net(double weights_ih[in+1][hid+1],double weights_ho[hid+1][out+1],double **images, double **targets,int thread_id);
+
+
 #endif /* openmp_h */
